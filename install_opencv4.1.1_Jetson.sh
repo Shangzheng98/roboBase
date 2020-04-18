1b5c6f40ad5139569e565fab1bf7413994cae9cf
Shangzheng98 <43415817+Shangzheng98@users.noreply.github.com>
2020-04-18 04:42:37 -0400

    Update install_opencv4.1.1_Jetson.sh

--- install_opencv4.1.1_Jetson.sh
+++ install_opencv4.1.1_Jetson.sh
@@ -28,11 +28,23 @@ sudo apt-get install -y python2.7-dev python3.6-dev python-dev python-numpy pyth
 sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libdc1394-22-dev
 sudo apt-get install -y libv4l-dev v4l-utils qv4l2 v4l2ucp
 sudo apt-get install -y curl
+sudo apt-get install -y python-pip
+sudo -H pip install -U jetson-stats
+sudo apt -y install libboost-all-dev
+sudo apt-get -y install vino
+echo "** Install boost lib successfully"
 sudo apt-get update            
 
+echo "** Installing glog"
+git clone https://github.com/google/glog.git
+cd glog
+cmake -H. -Bbuild -G "Unix Makefiles"
+cmake --build build
+sudo cmake --build build --target install
+cd ..
 
 echo "** Download opencv-4.1.1"
-cd $folder
+#cd $folder
 curl -L https://github.com/opencv/opencv/archive/4.1.1.zip -o opencv-4.1.1.zip
 curl -L https://github.com/opencv/opencv_contrib/archive/4.1.1.zip -o opencv_contrib-4.1.1.zip
 unzip opencv-4.1.1.zip
@@ -52,15 +64,5 @@ sudo make install
 #source ~/.bashrc
 echo "** Install opencv-4.1.1 successfully"
 
-sudo apt -y install libboost-all-dev
-echo "** Install boost lib successfully"
 
-
-echo "** Installing glog"
-git clone https://github.com/google/glog.git
-cd glog
-cmake -H. -Bbuild -G "Unix Makefiles"
-cmake --build build
-sudo cmake --build build --target install
 echo "** Bye :)"
-
