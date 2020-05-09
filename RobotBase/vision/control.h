10e57574653e7ac94aa7f5398fad07f9827dc767
jsz <shangj3@vt.edu>
2020-05-09 14:11:01 -0700

    add two enum type to detail the robot information

--- RobotBase/vision/control.h
+++ RobotBase/vision/control.h
@@ -2,6 +2,7 @@
 // Created by jsz on 12/27/19.
 //
 
+
 #ifndef ROBOTBASE_CONTROL_H
 #define ROBOTBASE_CONTROL_H
 #define ROI_ENABLE 1
@@ -13,4 +14,14 @@
 #define SHOW_ROI 1
 #define SHOW_DRAW_SPOT 0
 #define SHOW_LAST_TARGET 0
+
+typedef enum color {
+    BLUE, RED, UNKNOWN
+} Robot_color;
+
+typedef enum mode{
+    BIGBUFF, AUTOAIM, IDLE
+};
+
+
 #endif //ROBOTBASE_CONTROL_H
